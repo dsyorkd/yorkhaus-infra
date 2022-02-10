bb49f57273373fc3e75172a30454a5547473f20e
Spencer York <syork@yorkserv.com>
2022-02-10 01:30:09 -0600

    lots of changes

--- bootstrap/argocd/apply.sh
+++ bootstrap/argocd/apply.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
+
+echo "Deploying argocd"
 helm template \
     --include-crds \
     --namespace argocd \
     argocd . \
     | kubectl apply -n argocd -f -
 
+echo "Waiting..."
 kubectl -n argocd wait --timeout=60s --for condition=Established \
        crd/applications.argoproj.io \
        crd/applicationsets.argoproj.io
-       
+
+echo "done. \n"
+echo "logging in to argocd"
+
+passwd = kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+
+argocd login rancher.twistedlife.space/argocd/ --username admin --password $passwd
+
+
+argocd cluster add twisted-cluster
